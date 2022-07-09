e711ddc7365c7814fb4739d9e060174f98c76f48
Oleg Polivets <jsbot@ya.ru>
2022-07-09 10:53:50 +0300

    TestRunner: added ProcessQueries and ProcessQueriesJoined tests.

--- search-server/test_example_functions.cpp
+++ search-server/test_example_functions.cpp
@@ -17,6 +17,7 @@
 #include "request_queue.h"
 #include "remove_duplicates.h"
 #include "log_duration.h"
+#include "process_queries.h"
 
 using namespace std;
 
@@ -537,6 +538,62 @@ void TestSearchServer() {
     RUN_TEST(TestCalculationDocumentsRelevancy);
 }
 
+void TestProcessQueries() {
+    SearchServer search_server("and with"s);
+
+    int id = 0;
+    for (
+        const string& text : {
+            "funny pet and nasty rat"s,
+            "funny pet with curly hair"s,
+            "funny pet and not very nasty rat"s,
+            "pet with rat and rat and rat"s,
+            "nasty rat with curly hair"s,
+        }
+    ) {
+        search_server.AddDocument(++id, text, DocumentStatus::ACTUAL, {1, 2});
+    }
+
+    const vector<string> queries = {
+        "nasty rat -not"s,
+        "not very funny nasty pet"s,
+        "curly hair"s
+    };
+    id = 0;
+    for (
+        const auto& documents : ProcessQueries(search_server, queries)
+    ) {
+        cout << documents.size() << " documents for query ["s << queries[id++] << "]"s << endl;
+    }
+}
+
+void TestProcessQueriesJoined() {
+    SearchServer search_server("and with"s);
+
+    int id = 0;
+    for (
+        const string& text : {
+            "funny pet and nasty rat"s,
+            "funny pet with curly hair"s,
+            "funny pet and not very nasty rat"s,
+            "pet with rat and rat and rat"s,
+            "nasty rat with curly hair"s,
+        }
+    ) {
+        search_server.AddDocument(++id, text, DocumentStatus::ACTUAL, {1, 2});
+    }
+
+    const vector<string> queries = {
+        "nasty rat -not"s,
+        "not very funny nasty pet"s,
+        "curly hair"s
+    };
+    for (const Document& document : ProcessQueriesJoined(search_server, queries)) {
+        cout << "Document "s << document.id << " matched with relevance "s << document.relevance << endl;
+    }
+}
+
+
 void PrintDocument(const Document& document) {
     cout << "{ "s
          << "document_id = "s << document.id << ", "s
@@ -695,4 +752,12 @@ void AllTests() {
     RemoveDuplicates(search_server);
     cout << "After duplicates removed: "s << search_server.GetDocumentCount() << endl;
     }
+
+    cout << "//////////////////////////////////////////////////////////////" << endl;
+
+    TestProcessQueries();
+
+    cout << "//////////////////////////////////////////////////////////////" << endl;
+
+    TestProcessQueriesJoined();
 }
