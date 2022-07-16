6a206200bf375eed24cc654c4504e7ff3472639a
Oleg Polivets <jsbot@ya.ru>
2022-07-16 15:03:25 +0300

    UT: adding TestFindDocumentsParAndSeq

--- search-server/test_example_functions.cpp
+++ search-server/test_example_functions.cpp
@@ -765,6 +765,40 @@ void TestUsageOfStringView() {
     }
 }
 
+void TestFindDocumentsParAndSeq() {
+    SearchServer search_server("and with"s);
+
+    int id = 0;
+    for (
+        const string& text : {
+            "white cat and yellow hat"s,
+            "curly cat curly tail"s,
+            "nasty dog with big eyes"s,
+            "nasty pigeon john"s,
+        }
+    ) {
+        search_server.AddDocument(++id, text, DocumentStatus::ACTUAL, {1, 2});
+    }
+
+
+    cout << "ACTUAL by default:"s << endl;
+    // последовательная версия
+    for (const Document& document : search_server.FindTopDocuments("curly nasty cat"s)) {
+        PrintDocument(document);
+    }
+    cout << "BANNED:"s << endl;
+    // последовательная версия
+    for (const Document& document : search_server.FindTopDocuments(execution::seq, "curly nasty cat"s, DocumentStatus::BANNED)) {
+        PrintDocument(document);
+    }
+
+    cout << "Even ids:"s << endl;
+    // параллельная версия
+    for (const Document& document : search_server.FindTopDocuments(execution::par, "curly nasty cat"s, [](int document_id, DocumentStatus status, int rating) { return document_id % 2 == 0; })) {
+        PrintDocument(document);
+    }
+}
+
 void AllTests() {
     {
         TestSearchServer();
@@ -887,4 +921,8 @@ void AllTests() {
     cout << "//////////////////////////////////////////////////////////////" << endl;
 
     TestUsageOfStringView();
+
+    cout << "//////////////////////////////////////////////////////////////" << endl;
+
+    TestFindDocumentsParAndSeq();
 }
